1a11c5d26e18c5a6139a03eef3537cd0faf9b191
BjerregaardGG <oliverbjerregaard1@gmail.com>
2024-04-26 13:26:42 +0200

    Større dot på 30

--- SimpleSnake.java
+++ SimpleSnake.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 public class SimpleSnake extends JFrame {
 
     private static final int WINDOW_SIZE = 400;
-    private static final int DOT_SIZE = 20;
+    private static final int DOT_SIZE = 30;
     private int x = 100;
     private int y = 200;
 
